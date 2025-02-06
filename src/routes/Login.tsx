1f39d8f9d452a9f716830738087764ac15e21b3d
pooyan <thepooyan@gmail.com>
2025-02-06 11:33:42 +0330

    login page

---
+++ src/routes/Login.tsx
@@ -0,0 +1,11 @@
+const Login = () => {
+  return (
+    <form>
+      <input placeholder="Public key"/>
+      <input placeholder="Private key"/>
+      <button>Login</button>
+    </form>
+  )
+}
+
+export default Login
