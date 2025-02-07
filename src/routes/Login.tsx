4b88dab5de4d278a50e9bf210e7e4dcb4227b796
pooyan <thepooyan@gmail.com>
2025-02-07 18:47:28 +0330

    login update

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,11 +1,34 @@
+import { createSignal } from "solid-js"
 import { Button } from "~/components/ui/button"
+import { TextField, TextFieldInput, TextFieldLabel } from "~/components/ui/text-field"
 
 const Login = () => {
+
+  const [isLoading, setIsLoading] = createSignal(false)
+
+  let publicRef!: HTMLInputElement
+  let privateRef!: HTMLInputElement
+
+  const login = async () => {
+    setIsLoading(true)
+    console.log(publicRef.value, privateRef.value)
+    await new Promise((res) => setTimeout(res, 3000))
+    setIsLoading(false)
+  }
+
   return (
-    <form>
-      <input placeholder="Public key"/>
-      <input placeholder="Private key"/>
-      <Button>Login</Button>
+    <form class="w-sm flex flex-col gap-4 justify-center h-dvh m-auto ">
+      <TextField>
+        <TextFieldLabel>Public key</TextFieldLabel>
+        <TextFieldInput placeholder="Enter your Public key:" ref={publicRef}/>
+      </TextField>
+      <TextField>
+        <TextFieldLabel>Private key</TextFieldLabel>
+        <TextFieldInput placeholder="Enter your Private key:" ref={privateRef}/>
+      </TextField>
+      <Button onclick={login}>
+        {isLoading() ? "..." : "Login"}
+      </Button>
     </form>
   )
 }
