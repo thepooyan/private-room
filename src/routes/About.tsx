b62675315e1c2bb5e12805fa667c6986d8a2ae3c
pooyan <thepooyan@gmail.com>
2025-02-07 21:45:30 +0330

    add about signup dummy pages

---
+++ src/routes/About.tsx
@@ -0,0 +1,18 @@
+const About = () => {
+  return (
+    <div>
+      <p>This website was made using:</p>
+      <ul>
+        <li>Solid Start</li>
+        <li>Uno CSS</li>
+        <li>Pocketbase</li>
+        <li>Solid UI</li>
+        <li>RSA asymmetrical encryption with 256 curve</li>
+        <li>Pooyan Salmani</li>
+        <li>2025</li>
+      </ul>
+    </div>
+  )
+}
+
+export default About
