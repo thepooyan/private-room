108ad27d9c8ae124d18bd51334dcf1c597e242d3
pooyan <thepooyan@gmail.com>
2025-02-18 22:30:53 +0330

    nothing!

--- src/components/Layout/UserNav.tsx
+++ src/components/Layout/UserNav.tsx
@@ -1,16 +1,12 @@
 import { user } from "~/utility/signal";
 import Avatar from "../chat/Avatar";
-import { createEffect, Show } from "solid-js";
+import { Show } from "solid-js";
 import { Button } from "../ui/button";
 import Copyable from "../mine/Copyable";
 
 export const navHeight = 25
 
 const UserNav = () => {
-  createEffect(() => {
-
-  console.log(user.signal()?.public_key)
-  })
   return (
     <Show when={user.signal()}>
       {u => <div class={ ` px-5 p-2 bg-gray-200 flex justify-between items-center grid-col-span-full ` }>
