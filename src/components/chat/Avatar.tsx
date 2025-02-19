15667211337f0d6f7f094cb273dbca3648163b80
pooyan <thepooyan@gmail.com>
2025-02-19 17:09:45 +0330

    nothing!

--- src/components/chat/Avatar.tsx
+++ src/components/chat/Avatar.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "solid-js"
 import { avatarQuery } from "~/utility/queries"
 
 interface props {
@@ -5,9 +6,18 @@ interface props {
 }
 const Avatar = ({username}:props) => {
   let avatar = avatarQuery(username)
+
   return (
-    <div innerHTML={avatar.data} class="w-15 bg-gray rounded-full overflow-hidden" ></div>
+    <Suspense fallback={<Fallback/>}>
+      <div innerHTML={avatar.data} class="w-15 bg-gray rounded-full overflow-hidden" ></div>
+    </Suspense>
   )
+}
+
+const Fallback = () => {
+  return <div class="w-15 h-15 bg-gray rounded-full flex justify-center items-center" >
+    <div class="w-5 h-5 rounded-full border-2 border-white border-t-gray-300 border-b-gray-300 animate-spin"></div>
+  </div>
 }
 
 export default Avatar
