d06a062baf87cb3b5dff0986b67f70767fccd6e1
pooyan <thepooyan@gmail.com>
2025-02-22 00:49:56 +0330

    nothing!

--- src/components/chat/ChatView.tsx
+++ src/components/chat/ChatView.tsx
@@ -1,27 +1,28 @@
-import { Icontact } from "~/utility/interface"
-import Msg from "./Msg"
-import { api } from "~/utility/backend"
-import { Accessor, For, Show, Suspense } from "solid-js"
+import { Icontact } from "~/utility/interface";
+import Msg from "./Msg";
+import { api } from "~/utility/backend";
+import { Accessor, For, Show, Suspense } from "solid-js";
 
 interface props {
-  c: Accessor<Icontact>
+  c: Accessor<Icontact>;
 }
-const ChatView = ({c}:props) => {
-
-  const { signal } = api.messages.getLiveResource(c)
+const ChatView = ({ c }: props) => {
+  const { signal } = api.messages.getLiveResource(c);
 
   return (
     <Suspense fallback="...">
-    <div class="p-5 px-7">
-      <Show when={signal?.data}>
-        {ms => <For each={ms().items}>{i => <Msg isRightSide={i.sender !== c().id}>{i.content}</Msg>}</For>}
-      </Show>
-      <Show when={signal?.data?.items.length === 0}>
-        "No messages yet!"
-      </Show>
-    </div>
+      <div class="p-5 px-7 overflow-auto h-[69dvh]">
+        <Show when={signal?.data}>
+          {(ms) => (
+            <For each={ms().items}>
+              {(i) => <Msg isRightSide={i.sender !== c().id}>{i.content}</Msg>}
+            </For>
+          )}
+        </Show>
+        <Show when={signal?.data?.items.length === 0}>"No messages yet!"</Show>
+      </div>
     </Suspense>
-  )
-}
+  );
+};
 
-export default ChatView
+export default ChatView;
