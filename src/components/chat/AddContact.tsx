c15ab282632fc1d7d3ffdf914e09b3f1b6c8d912
pooyan <thepooyan@gmail.com>
2025-02-25 14:00:53 +0330

    nothing!

--- src/components/chat/AddContact.tsx
+++ src/components/chat/AddContact.tsx
@@ -1,9 +1,7 @@
 import { AiFillPlusCircle } from "solid-icons/ai";
 import { Iuser } from "~/utility/interface";
-import { contanctStorage, debounce } from "~/utility/utility";
-import { reloadContacts } from "./contactList";
+import { debounce } from "~/utility/utility";
 import { api } from "~/utility/backend";
-import { setCurrentChat } from "~/utility/signal";
 import {
   AlertDialog,
   AlertDialogContent,
@@ -19,6 +17,8 @@ import Spinner from "../mine/Spinner";
 const AddContact = () => {
   const [result, setResult] = createSignal<Iuser[]>([]);
   const [searching, setSearching] = createSignal(false);
+  const [open, setOpen] = createSignal(false);
+  
 
   const searchRequest = async (str: string) => {
     let a = await api.users.searchByUsername(str);
@@ -33,36 +33,17 @@ const AddContact = () => {
     setSearching(true);
     d_searchReq(str);
   };
-  const add = async () => {
-    let pk = prompt("Public key of the guy:");
-    let trg: Iuser;
-    if (!pk) return;
 
-    try {
-      trg = await api.users.findByPK(pk);
-    } catch (e) {
-      return alert("User not found!");
-    }
-
-    let newcontact = {
-      username: trg.username,
-      public_key: trg.public_key,
-      id: trg.id,
-    };
-    contanctStorage.add(newcontact);
-    reloadContacts();
-    setCurrentChat(newcontact);
-  };
   return (
     <>
-      <AlertDialog>
+      <AlertDialog open={open()} onOpenChange={setOpen}>
         <AlertDialogTrigger class="w-full">
           <AiFillPlusCircle class="text-gray-400 hover:text-gray-500 cursor-pointer w-5 h-5 rounded-full flex justify-center items-center font-bold mx-auto my-2 " />
         </AlertDialogTrigger>
         <AlertDialogContent>
           <AlertDialogTitle>Add contacts:</AlertDialogTitle>
           <AlertDialogDescription>
-            You can search for people by their username, and add them to your contacts.
+            Search people by their username, and add them to your contacts.
           </AlertDialogDescription>
 
           <div class="space-y-2">
@@ -74,7 +55,7 @@ const AddContact = () => {
             </TextField>
             <div class=" max-h-60 overflow-auto ">
               <For each={result()}>
-                {(r) => <SearchItemPreview contact={r} />}
+                {(r) => <SearchItemPreview close={()=>setOpen(false)} contact={r}/>}
               </For>
             </div>
             <Show when={searching()}>
--- src/components/chat/SearchItemPreview.tsx
+++ src/components/chat/SearchItemPreview.tsx
@@ -2,13 +2,25 @@ import { Iuser } from "~/utility/interface"
 import { AiOutlineUserAdd } from 'solid-icons/ai'
 import Avatar from "./Avatar"
 import clsx from "clsx"
+import { contanctStorage } from "~/utility/utility"
+import { reloadContacts } from "./contactList"
+import { setCurrentChat } from "~/utility/signal"
 
 interface props {
   contact: Iuser
+  close: ()=>void
 }
-const SearchItemPreview = ({contact}:props) => {
+const SearchItemPreview = ({contact, close}:props) => {
+  const addMe = () => {
+    close()
+    contanctStorage.add(contact);
+    reloadContacts();
+    setCurrentChat(contact);
+  }
   return (
-    <div class={clsx(`flex rounded-lg m-1 cursor-pointer items-center gap-2 cursor-pointer hover:bg-gray-600 p-2 transition-colors border-1 border-gray-800 `)}>
+    <div class={clsx(`flex rounded-lg m-1 cursor-pointer items-center gap-2 cursor-pointer hover:bg-gray-600 p-2 transition-colors border-1 border-gray-800 `)}
+      onclick={addMe}
+    >
       <AiOutlineUserAdd class=" text-green-400 "/>
       <Avatar username={contact.username} sm/>
       <p class="ml-1">{contact.username}</p>
