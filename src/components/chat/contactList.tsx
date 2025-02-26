1874c40fcbee4b49e0f8d30861e98cf72f982beb
pooyan <thepooyan@gmail.com>
2025-02-26 15:17:54 +0330

    nothing!

--- src/components/chat/contactList.tsx
+++ src/components/chat/contactList.tsx
@@ -2,6 +2,7 @@ import { createSignal, For, Show } from "solid-js"
 import { contanctStorage } from "~/utility/utility"
 import ContactPreview from "./ContactPreview"
 import AddContact from "./AddContact"
+import { AiFillPlusCircle } from "solid-icons/ai"
 
 let [contacts, setContacts] = createSignal(contanctStorage.getAll())
 export const reloadContacts = () => {
@@ -16,9 +17,11 @@ const ContactList = () => {
           {c => <ContactPreview contact={c}/>}
         </For>
         <Show when={contacts().size === 0}>
-          <p class="text-center text-sm p-2 text-zinc-300">
+          <p class="text-center text-sm p-4 text-zinc-400 absolute bottom-[50%]">
             You have no contacts yet...
-            You can add people to chat with, using the plus icon
+            You can add people to chat with, using the (
+            <AiFillPlusCircle class="inline"/>
+            ) icon.
           </p>
         </Show>
       </Show>
